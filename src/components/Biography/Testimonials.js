451ef6e092c12c70f3054a01b351e5f9f3371fba
jsusilo <enr@bk.ru>
2021-03-20 00:21:11 +0500

    Biography Testimonials comp

---
+++ src/components/Biography/Testimonials.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+class Testimonials extends React.Component {
+  render() {
+    return (
+      <section className="section section-lg bg-secondary-2">
+        <div className="container">
+          <div className="text-center text-sm-left">
+            <h2>Testimonials</h2>
+          </div>
+          <div className="row row-50">
+            <div className="col-lg-3 col-sm-6">
+              <div className="quote-classic">
+                <div className="quote-classic-quote">“</div>
+                <p className="quote-classic-text">
+                  Elit, sed do eiusmod Incididunt ut labore et dolore magna
+                  aliqua. Ut enim ad minim veniam quis nostrud exercitation
+                  ullamco laboris nisUt rutrum lobortis odio, ut interdum tortor
+                  laoreet et. Donec eget lectus
+                </p>
+                <h6 className="quote-classic-author">ANETE PUGA</h6>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="quote-classic">
+                <div className="quote-classic-quote">“</div>
+                <p className="quote-classic-text">
+                  Ut enim ad minim veniam quis no strud exercitation ullamco
+                  labo ris nisUt rutrum lobortis odio, ut interdum tortor
+                  laoreet et. Don ec eget lectus dui. Integer vel fa ucibus
+                  risus. Mauris dolor
+                </p>
+                <h6 className="quote-classic-author">CAROLINE BEEK</h6>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="quote-classic">
+                <div className="quote-classic-quote">“</div>
+                <p className="quote-classic-text">
+                  Donec eget lectus dui. Integer vel faucibus risus. Mauris
+                  dolor enim, fringilla sed molestie at, faucibus vel purus.
+                  Aliquam vulputate ac orci commodo molestie. Cras vehicula,
+                  dolor vitae sollicitudin place
+                </p>
+                <h6 className="quote-classic-author">DAYLE PETERS</h6>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="quote-classic">
+                <div className="quote-classic-quote">“</div>
+                <p className="quote-classic-text">
+                  Binim veniam quis nostrud exercitation ullamco laboris nisUt
+                  rutrum lobortis odio, ut interdum tortor laoreet et. Donec
+                  eget lectus dui. Integer vel faucibus risus. Mauris dolor
+                  enim, fringill
+                </p>
+                <h6 className="quote-classic-author">EDNA BARTON</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Testimonials;
