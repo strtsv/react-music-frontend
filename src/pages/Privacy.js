971be1ddbf8a27e495ad14339dc7dc1fe9f7a756
jsusilo <enr@bk.ru>
2021-03-22 22:12:51 +0500

    privacy page

---
+++ src/pages/Privacy.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/Privacy/Header";
+import Content from "../components/Privacy/Content";
+import Footer from "../components/Privacy/Footer";
+
+const Privacy = () => {
+  return (
+    <div className="page">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+};
+
+export default Privacy;
